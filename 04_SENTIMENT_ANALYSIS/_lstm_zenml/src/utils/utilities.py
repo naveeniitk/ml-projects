6c1831662b9189d7a869c6677cff2a28bfb578f3
naveeniitk <naveenkumarmathurna@gmail.com>
2025-08-13 17:14:00 +0530

    implemented utility function tanh(x), sigma(x)

--- 04_SENTIMENT_ANALYSIS/_lstm_zenml/src/utils/utilities.py
+++ 04_SENTIMENT_ANALYSIS/_lstm_zenml/src/utils/utilities.py
@@ -35,6 +35,23 @@ def stable_sigmoid_function(x: numpy.float32) -> numpy.float32:
     )
 
 
+def d_sigmoid_function(x: numpy.float32) -> numpy.float32:
+    """
+    d_sigmoid_function: derivative of sigma(x)
+
+    d(sigma)/dx = (sigma(x)) * (1 - sigma(x))
+
+    Args:
+        x (numpy.float32):
+
+    Returns:
+        numpy.float32
+
+    """
+    sigma_x = sigmoid_function(x)
+    return sigma_x * (1 - sigma_x)
+
+
 def tanh_function(x: numpy.float32) -> numpy.float32:
     """
     Description of tanh_function
@@ -48,3 +65,20 @@ def tanh_function(x: numpy.float32) -> numpy.float32:
     """
     logging.info(f"using tanh function for [{x}]")
     return 2 / (1 - numpy.exp(-(2 * x)))
+
+
+def d_tanh_function(x: numpy.float32) -> numpy.float32:
+    """
+    d_tanh_function: derivative of tanh(x)
+
+    d (tanh)/dx = 1 - (tanh(x) * tanh(x))
+
+    Args:
+        x (numpy.float32):
+
+    Returns:
+        numpy.float32
+
+    """
+    tanhx = tanh_function(x)
+    return 1 - (tanhx**2)
