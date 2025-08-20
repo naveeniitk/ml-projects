c0225b211d127f35e801c39d8383cdd8cd57b503
naveeniitk <naveenkumarmathurna@gmail.com>
2025-08-20 18:23:59 +0530

    type specified for the model after completion of the training step

--- 04_SENTIMENT_ANALYSIS/_lstm_zenml/src/pipelines/lstm_pipeline.py
+++ 04_SENTIMENT_ANALYSIS/_lstm_zenml/src/pipelines/lstm_pipeline.py
@@ -7,7 +7,7 @@ from config.params import DATA_PATH
 from steps.importer_step import importer_step
 from steps.preprocess_step import preprocess_step
 from steps.trainer_step import trainer_step
-
+from models.lstm import LstmClassifier
 
 @pipeline(enable_cache=False)
 def lstm_pipeline() -> None:
@@ -37,19 +37,15 @@ def lstm_pipeline() -> None:
     # =====================================================================
     logging.info("Training and testing data split successfully.")
     X_train, X_test, y_train, y_test = preprocessed_data
-
-    # logging.info(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
-    # logging.info(f"X_test shape: {X_test.shape}, y_test shape: {y_test.shape}")
-
-    # logging.info(f"X_train [0] shape: {X_train[0].shape}")
-    # logging.info(f"y_train [0] : {y_train.iloc[0]}")
-
+    
     # =====================================================================
     logging.info("Starting the training step...")
     try:
-        lstm_model: Any = trainer_step(X_train, y_train)
+        lstm_model: LstmClassifier = trainer_step(X_train, y_train)
         logging.info("LSTM Model trained successfully.")
     except Exception as e:
         logging.error(f"An error occurred during the training step: {e}")
         raise e
     logging.info("Training step completed successfully.")
+    
+    lstm_model
